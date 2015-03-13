db8b2843873832e745e5684baa73bda62e7a282b
FilterSelect <joe@jdsl.me>
2015-03-13 14:16:11 +1100

    First code!

---
+++ tonez.py
@@ -0,0 +1,12 @@
+def tonydumbdumb(words):
+    if len(words) > 0:
+        print "Oh dear, that was pretty dumb."
+        return True
+    else:
+        print "Bliss!"
+        return False
+
+words = raw_input("What did Tonez say? ")
+
+tonydumbdumb(words)
+
